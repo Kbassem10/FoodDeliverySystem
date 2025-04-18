4aecd6a956f413229acea6c48bf204f7c46aa171
Karim Bassem <kimobisso@gmail.com>
2025-04-18 21:17:01 +0200

    add MenuItemsController and OrderController for improved menu and order management

---
+++ src/main/java/com/fooddeliverysystem/controller/MenuItemsController.java
@@ -0,0 +1,93 @@
+package com.fooddeliverysystem.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fooddeliverysystem.model.MenuItems;
+
+public class MenuItemsController {
+    
+    private List<MenuItems> menuItemsList;
+
+    private static MenuItemsController instance;
+    
+    private MenuItemsController() {
+        menuItemsList = new ArrayList<>();
+        initializeMenuItems();
+    }
+    
+    public static MenuItemsController getInstance() {
+        if (instance == null) {
+            instance = new MenuItemsController();
+        }
+        return instance;
+    }
+    
+    private void initializeMenuItems() {
+        // Add some sample menu items
+        addMenuItem(new MenuItems("P1", "Pizza Margherita", 9.99, "Pizza"));
+        addMenuItem(new MenuItems("P2", "Pizza Pepperoni", 11.99, "Pizza"));
+        addMenuItem(new MenuItems("B1", "Cheeseburger", 7.99, "Burger"));
+        addMenuItem(new MenuItems("D1", "Cola", 1.99, "Drink"));
+        addMenuItem(new MenuItems("D2", "Water", 0.99, "Drink"));
+    }
+    
+    public List<MenuItems> getAllMenuItems() {
+        return new ArrayList<>(menuItemsList);
+    }
+
+    public boolean addMenuItem(MenuItems menuItem) {
+        if (menuItem != null && !menuItemExists(menuItem.getItemId())) {
+            return menuItemsList.add(menuItem);
+        }
+        return false;
+    }
+    
+    /**
+     * Get menu item by ID
+     * Similar to MenuItems.objects.get(itemId=id) in Django
+     */
+    public MenuItems getMenuItemById(String itemId) {
+        return menuItemsList.stream()
+                .filter(item -> item.getItemId().equals(itemId))
+                .findFirst()
+                .orElse(null);
+    }
+    
+    /**
+     * Update menu item
+     * Similar to item = MenuItems.objects.get(itemId=id); item.name = ...; item.save()
+     */
+    public boolean updateMenuItem(String itemId, String name, double price, String category) {
+        MenuItems item = getMenuItemById(itemId);
+        if (item != null) {
+            item.setName(name);
+            item.setPrice(price);
+            item.setCategory(category);
+            return true;
+        }
+        return false;
+    }
+    
+    /**
+     * Delete menu item
+     * Similar to MenuItems.objects.get(itemId=id).delete() in Django
+     */
+    public boolean deleteMenuItem(String itemId) {
+        MenuItems item = getMenuItemById(itemId);
+        if (item != null) {
+            return menuItemsList.remove(item);
+        }
+        return false;
+    }
+    
+    /**
+     * Check if menu item exists
+     * Similar to MenuItems.objects.filter(itemId=id).exists() in Django
+     */
+    public boolean menuItemExists(String itemId) {
+        return menuItemsList.stream()
+                .anyMatch(item -> item.getItemId().equals(itemId));
+    }
+
+}
---
+++ src/main/java/com/fooddeliverysystem/controller/OrderController.java
@@ -0,0 +1,160 @@
+package com.fooddeliverysystem.controller;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import com.fooddeliverysystem.model.MenuItems;
+import com.fooddeliverysystem.model.Order;
+
+/**
+ * OrderController handles operations related to customer orders.
+ * Similar to Django views, this controller processes requests related to orders
+ * but doesn't render templates (JavaFX handles the view separately).
+ */
+public class OrderController {
+    
+    // In-memory storage of orders (similar to Django's ORM but in memory)
+    private Map<String, Order> orders;
+    
+    // Reference to the MenuItemsController (like importing another view/service in Django)
+    private MenuItemsController menuItemsController;
+    
+    // Singleton instance
+    private static OrderController instance;
+    
+    // Current active order (similar to session-based cart in Django)
+    private Order currentOrder;
+    
+    /**
+     * Private constructor
+     * In Django, views are instantiated by the framework
+     */
+    private OrderController() {
+        orders = new HashMap<>();
+        menuItemsController = MenuItemsController.getInstance();
+        initializeCurrentOrder();
+    }
+    
+    /**
+     * Get singleton instance
+     * In Django, you wouldn't need this as views are instantiated by the framework
+     */
+    public static OrderController getInstance() {
+        if (instance == null) {
+            instance = new OrderController();
+        }
+        return instance;
+    }
+    
+    /**
+     * Initialize a new current order
+     * Similar to creating a new cart in Django session
+     */
+    public void initializeCurrentOrder() {
+        currentOrder = new Order();
+    }
+    
+    /**
+     * Get the current active order
+     * Similar to getting the cart from session in Django
+     */
+    public Order getCurrentOrder() {
+        return currentOrder;
+    }
+    
+    /**
+     * Add item to current order
+     * Similar to adding an item to cart in Django
+     */
+    public void addItemToCurrentOrder(String menuItemId) {
+        MenuItems item = menuItemsController.getMenuItemById(menuItemId);
+        if (item != null) {
+            currentOrder.addItem(item);
+        }
+    }
+    
+    /**
+     * Remove item from current order
+     * Similar to removing an item from cart in Django
+     */
+    public boolean removeItemFromCurrentOrder(String menuItemId) {
+        MenuItems item = menuItemsController.getMenuItemById(menuItemId);
+        if (item != null) {
+            return currentOrder.removeItem(item);
+        }
+        return false;
+    }
+    
+    /**
+     * Submit the current order
+     * Similar to checkout process in Django
+     * return orderId of the submitted order
+     */
+    public String submitOrder() {
+        if (currentOrder.getOrderItems().isEmpty()) {
+            return null;
+        }
+        
+        String orderId = generateOrderId();
+        orders.put(orderId, currentOrder);
+        
+        // Create a new current order (like clearing the cart after checkout)
+        initializeCurrentOrder();
+        
+        return orderId;
+    }
+    
+    /**
+     * Generate a unique order ID
+     * Similar to using UUID in Django models
+     */
+    private String generateOrderId() {
+        return UUID.randomUUID().toString();
+    }
+    
+    /**
+     * Get an order by its ID
+     * Similar to Order.objects.get(id=orderId) in Django
+     */
+    public Order getOrder(String orderId) {
+        return orders.get(orderId);
+    }
+    
+    /**
+     * Get all orders
+     * Similar to Order.objects.all() in Django
+     */
+    public List<Order> getAllOrders() {
+        return new ArrayList<>(orders.values());
+    }
+    
+    /**
+     * Update customer information in the current order
+     * Similar to updating form data in Django
+     */
+    public void updateCustomerInfo(String name, String address, String phone) {
+        currentOrder.setCustomerName(name);
+        currentOrder.setCustomerAddress(address);
+        currentOrder.setCustomerPhone(phone);
+    }
+    
+    /**
+     * Cancel the current order
+     * Similar to clearing the cart in Django
+     */
+    public void cancelCurrentOrder() {
+        currentOrder.cancelOrder();
+    }
+    
+    /**
+     * Get the total price of the current order
+     * Similar to calculating cart total in Django
+     */
+    public double getCurrentOrderTotal() {
+        return currentOrder.getTotalPrice();
+    }
+    
+}
